f6a638baf5b35f73f0f4db963b59f41b6ed6f762
vangeytedvg@gmail.com <vangeytedvg>
2023-03-23 11:17:41 +0100

    Documentation Commit

--- frmmain.pas
+++ frmmain.pas
@@ -5,7 +5,7 @@ unit frmMain;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ComObj, variants;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ComObj, variants, ActiveX;
 
 { TTemplate will be used to hold all fields to be replaced and their new values }
 type
@@ -21,8 +21,12 @@ type
   TMainForm = class(TForm)
     btnCreateLibreDoc: TButton;
     btnReport: TButton;
+    btnInsertTextAndNewLine: TButton;
+    btnGotoLine: TButton;
     procedure btnCreateLibreDocClick(Sender: TObject);
+    procedure btnGotoLineClick(Sender: TObject);
     procedure btnReportClick(Sender: TObject);
+    procedure btnInsertTextAndNewLineClick(Sender: TObject);
   private
 
   public
@@ -44,8 +48,8 @@ const
   ServerName = 'com.sun.star.ServiceManager';
 var
   Server: variant;
-  Desktop, TextDocument, CoreReflection, HeaderRow, LoadParams, TextCursor, MyText,
-  CellCursor, Table, Cell: variant;
+  Desktop, TextDocument, CoreReflection, HeaderRow, LoadParams, TextCursor,
+  MyText, CellCursor, Table, Cell: variant;
   FontName: string;
   FileName: string;
   i, j: integer;
@@ -63,8 +67,7 @@ begin
 
   // Desktop init
   Desktop := Server.CreateInstance('com.sun.star.frame.Desktop');
-  CoreReflection := Server.CreateInstance(
-    'com.sun.star.reflection.CoreReflection');
+  CoreReflection := Server.CreateInstance('com.sun.star.reflection.CoreReflection');
 
   // Initialize the Service Manager
   Server := CreateOleObject(ServerName);
@@ -75,7 +78,7 @@ begin
   MyText := TextDocument.getText;
   // Assign a cursor
   TextCursor := MyText.createTextCursor;
-    FontName := 'Arial';
+  FontName := 'Arial';
   TextCursor.CharFontName := FontName;
   TextCursor.CharColor := $AAEECC;
   TextCursor.CharWeight := 150; // Make it bold
@@ -112,11 +115,43 @@ begin
 
 end;
 
-procedure TMainForm.btnReportClick(Sender: TObject);
-{ Replace a field in the template report }
+procedure TMainForm.btnGotoLineClick(Sender: TObject);
 var
-   LOInstance, LOComponent, Textt, RText, LoadParams, TextBody, SearchDescriptor: Variant;
-   newFileName, templateFileName: string;
+  ServiceManager: olevariant;
+  Desktop: olevariant;
+  TextDocument: olevariant;
+  Text_: olevariant;
+  Cursor_: olevariant;
+  i: integer;
+begin
+  // create the service manager and get the desktop
+  ServiceManager := CreateOleObject('com.sun.star.ServiceManager');
+  Desktop := ServiceManager.createInstance('com.sun.star.frame.Desktop');
+
+  // load a new text document
+  TextDocument := Desktop.loadComponentFromURL('private:factory/swriter',
+    '_blank', 0, VarArrayCreate([0, -1], varVariant));
+  Text_ := TextDocument.getText();
+
+  // insert 10 lines of text
+  for i := 1 to 10 do
+  begin
+    Text_.insertString(Text_.getEnd, 'Line ' + IntToStr(i) + #13, False);
+  end;
+
+  // move the cursor to the start of line 5
+  Cursor_ := Text_.createTextCursorByRange(Text_.getStart());
+  Cursor_.gotoRange(Text_.getStart(), True);
+//  Cursor_.;
+end;
+
+procedure TMainForm.btnReportClick(Sender: TObject);
+{
+ Replace a field in the template report
+}
+var
+  LOInstance, LOComponent, Text_, Cursor_, LoadParams, TextBody, ovc: variant;
+  newFileName, templateFileName: string;
 begin
 
   templateFileName := 'file:///C:/temp/template.odt';
@@ -128,15 +163,64 @@ begin
   LOComponent := LOInstance.createInstance('com.sun.star.frame.Desktop');
 
   // Load the template document
-  TextBody := LOComponent.loadComponentFromURL(templateFileName, '_blank', 0, LoadParams);
+  TextBody := LOComponent.loadComponentFromURL(templateFileName,
+    '_blank', 0, LoadParams);
+
+
   // Get the body of the document
-  RText := TextBody.createReplaceDescriptor;
-  RText.setSearchString('{NAME}');
-  RText.setReplaceString('Danny Van Geyte');
-  TextBody.ReplaceAll(RText);
+  Text_ := TextBody.createReplaceDescriptor;
+  Text_.setSearchString('{NAME}');
+  Text_.setReplaceString('Danny Van Geyte');
+  TextBody.ReplaceAll(Text_);
 
   // Save the new file
   TextBody.storeToURL(newFileName, VarArrayCreate([0, 0], varVariant));
+  TextBody.Close(True);
+  // Now load the newly created document
+  TextBody := LOComponent.loadComponentFromURL(newFileName, '_blank', 0, LoadParams);
+  Text_ := TextBody.getText();
+
+  oVC := TextBody.getCurrentController.getViewCursor;
+
+  Cursor_ := Text_.createTextCursorByRange(oVC);
+  oVC.JumpToStartOfPage;
+  Cursor_.gotoRange(oVC, true);
+  oVC.goDown(8, false);
+//  Cursor_.gotoRange(ovc, false);
+//  Cursor_.gotoPreviousWord(true);
+  Text_.insertString(oVC.getStart(), 'JWAAAAJ', False);
+end;
+
+procedure TMainForm.btnInsertTextAndNewLineClick(Sender: TObject);
+{
+ Insert new lines in a text document
+}
+var
+  ServiceManager: variant;
+  LoadParams: variant;
+  Desktop: variant;
+  TextDocument: variant;
+  Text_: variant;
+  Frame: variant;
+  Table: variant;
+  CellRange: variant;
+  Cursor_: variant;
+  i, j: integer;
+begin
+  LoadParams := VarArrayCreate([0, -1], varVariant);
+  ServiceManager := CreateOleObject('com.sun.star.ServiceManager');
+  Desktop := ServiceManager.createInstance('com.sun.star.frame.Desktop');
+  TextDocument := Desktop.loadComponentFromURL('private:factory/swriter',
+    '_blank', 0, LoadParams);
+  Text_ := TextDocument.Text;
+
+  // Insert some text after the table
+  Text_.insertString(Text_.getEnd, 'This is some text.', False);
+  Text_.insertString(Text_.getEnd, #10#10, False);
+  Text_.insertString(Text_.getEnd, 'This is some text.', False);
+
+  Frame := TextDocument.CurrentController.Frame;
+  Frame.activate;
 
 end;
 
